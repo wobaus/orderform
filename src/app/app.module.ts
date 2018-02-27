b94bd44608ff0e7d1c55810ba60c51d1d840267f
Sun Baus <wobaus@gmail.com>
2018-02-26 22:06:42 -0800

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MenuListComponent } from './menu-bar/menu-list/menu-list.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'breakfast', component: MenuListComponent},
+  { path: 'lunch', component: MenuListComponent},
 
+];
 @NgModule({
   declarations: [
     AppComponent,
