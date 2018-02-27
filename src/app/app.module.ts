9aa900ada72eab701c86a26316df2e49f7dff277
Sun Baus <wobaus@gmail.com>
2018-02-26 21:17:51 -0800

    basic components set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenuListComponent } from './menu-bar/menu-list/menu-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuBarComponent,
+    MenuListComponent
   ],
   imports: [
     BrowserModule
--- src/app/breakfast-menus.service.ts
+++ src/app/breakfast-menus.service.ts
@@ -1,6 +1,6 @@
 import { MenuItem } from './shared/menu-item.model';
 
-export class Menus {
+export class BreakfastMenus {
     private menu_items: MenuItem[] = [
         new MenuItem ('pancake', 10),
         new MenuItem ('chicken & waffle', 15)
