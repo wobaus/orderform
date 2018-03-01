2137273407b55f7dfe3d7de9bf39a97a357346d4
Sun Baus <wobaus@gmail.com>
2018-02-28 23:35:39 -0800

    adding lunch/dinner services

---
+++ src/app/dinner-menus.service.ts
@@ -0,0 +1,8 @@
+import { MenuItem } from './shared/menu-item.model';
+
+export class DinnerMenus {
+    private menu_items: MenuItem[] = [
+        new MenuItem ('lamb Shank', 28),
+        new MenuItem ('Fish & Chip', 21)
+    ];
+}
---
+++ src/app/lunch-menus.service.ts
@@ -0,0 +1,8 @@
+import { MenuItem } from './shared/menu-item.model';
+
+export class LunchMenus {
+    private menu_items: MenuItem[] = [
+        new MenuItem ('bongole pasta', 25),
+        new MenuItem ('ceasar salad', 18)
+    ];
+}
